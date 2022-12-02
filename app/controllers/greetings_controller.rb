3e10734e0d1c509ee7d42425d960288ac6ff9e2e
vanessaBonito <vanebonito@hotmail.com>
2022-12-02 14:49:02 -0500

    Add endpoint

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 class GreetingsController < ApplicationController
-  before_action :set_greeting, only: %i[ show update destroy ]
+  before_action :set_greeting, only: %i[show update destroy]
 
   # GET /greetings
   def index
-    @greetings = Greeting.all
-
-    render json: @greetings
+    @greeting = Greeting.order('RANDOM()').first
+    render json: @greeting
   end
 
   # GET /greetings/1
@@ -39,13 +40,14 @@ class GreetingsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_greeting
-      @greeting = Greeting.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def greeting_params
-      params.require(:greeting).permit(:message)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_greeting
+    @greeting = Greeting.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def greeting_params
+    params.require(:greeting).permit(:message)
+  end
 end
