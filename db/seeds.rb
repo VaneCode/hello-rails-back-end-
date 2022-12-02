939d6a3a81244126c433223d9a0d401cad425c0e
vanessaBonito <vanebonito@hotmail.com>
2022-12-02 14:41:29 -0500

    Create table greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(message: "Hello")
+Greeting.create(message: "Hola")
+Greeting.create(message: "Selam dyu")
+Greeting.create(message: "Nin Hao")
+Greeting.create(message: "Bonjour")
